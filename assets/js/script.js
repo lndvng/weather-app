c3a90584010a025b277a0cfe3741c84997ba72a2
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-09-05 11:46:23 -0700

    added search history buttons and 5 day section now generates info from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,9 +28,10 @@ function displayWeatherData(weatherData) {
     document.getElementById('temp').textContent = 'Temperature: ' + temperature +'\u2109';
     document.getElementById('humidity').textContent = 'Humidity: ' + humidity + "%";
     document.getElementById('windSpeed').textContent = 'Wind Speed: ' + windSpeed + 'MPH';
-    }
+}
 
 // request API for five day container
+
 function getForecastData(city) {
     let apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=9997ba272b04892d57f7add3dd35b249&units=imperial';
     
@@ -47,32 +48,32 @@ fetch(apiUrl)
 // function to display 5 day weather data in five day container
 function displayForecastData(forecastData) {
 
-    // for loop to iterate through 5 days of forecast
-    for (let i = 0; i < 5; i++) {
-    let forecastItem = forecastData.list[i];
+    let dateEls = document.getElementsByClassName('date');
+    let iconEls = document.getElementsByClassName('icon');
+    let tempEls = document.getElementsByClassName('temp');
+    let humidityEls = document.getElementsByClassName('humidity');
+    let windSpeedEls = document.getElementsByClassName('windSpeed');
 
-    let dateEl = document.getElementsByClassName('date');
-    let iconEl = document.getElementsByClassName('icon');
-    let tempEl = document.getElementsByClassName('temp');
-    let humidityEl = document.getElementsByClassName('humidity');
-    let windSpeedEl = document.getElementsByClassName('windSpeed');
+    // for loop to iterate through 5 days of forecast
+    for (let i = 0; i < dateEls.length; i++) {
+        let forecastItem = forecastData.list[i*8];
 
     // define variables from data extracted from API
-    let dateTime = forecastItem.dt_txt;
+    let dateTime = forecastItem.dt_txt.slice(0, 10);
     let iconCode = forecastItem.weather[0].icon;
     let temperature = forecastItem.main.temp;
     let humidity = forecastItem.main.humidity;
     let windSpeed = forecastItem.wind.speed;
 
     // display data from API to 5 day container
-    dateEl.textContent = dateTime;
-    iconEl.textContent = 'Icon: ' + iconCode;
-    tempEl.textContent = 'Temp: ' + temperature + '\u2109';
-    humidityEl.textContent = 'Humidity: ' + humidity + "%";
-    windSpeedEl.textContent = 'Wind: ' + windSpeed + 'MPH';
-
+    dateEls[i].textContent = dateTime;
+    iconEls[i].textContent = '\u2600';
+    tempEls[i].textContent = 'Temp: ' + temperature + '\u2109';
+    humidityEls[i].textContent = 'Humidity: ' + humidity + "%";
+    windSpeedEls[i].textContent = 'Wind: ' + windSpeed + 'MPH';
     }
 }
+
 
 // event listener for search button to generate forecast and five day information from API on click
 button.addEventListener('click', function (event) {
@@ -80,4 +81,7 @@ button.addEventListener('click', function (event) {
     const cityEntry = document.querySelector('.form-control').value;
     getWeatherData(cityEntry);
     getForecastData(cityEntry);
-    });
+
+    localStorage.setItem('city', cityEntry);
+});
+
